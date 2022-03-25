459a6a0cbb7bf1cbbec0ecc01e39d33ebe83a652
Ju-yeop <dhwnduq12@naver.com>
2022-03-25 21:48:49 +0900

    Auth

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react"
+import AppRouter from "components/Router";
+import {authService} from "fbase";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user){
+        setIsLoggedIn(true);
+        const uid = user.uid;
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+    </>
+  )
+}
+
+export default App;
