dd5dc7f6f801710d77c13fe0ea3bc9d256f99ead
Ju-yeop <dhwnduq12@naver.com>
2022-03-27 23:32:35 +0900

    Tweet

---
+++ src/components/Tweet.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { dbService } from "fbase";
+import { useState } from "react";
+
+const Tweet = ({tweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+    const onChange = (event) => {
+        const {target:{value}} = event;
+        setNewTweet(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`tweets/${tweetObj.id}`).update({
+            text:newTweet
+        })
+        setEditing(false);
+    }
+    const deleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this nweet?");
+        if (ok){
+            await dbService.doc(`tweets/${tweetObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    return(
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input value={newTweet} placeholder="Edit your Tweet" type="text" required onChange={onChange}/>
+                        <input value="Update Tweet" type="submit" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </> ):(
+                <>
+                    <div>
+                        <h4>{tweetObj.text}</h4>
+                        {isOwner && (
+                            <>
+                                <button onClick={deleteClick}>Delete Tweet</button>
+                                <button onClick={toggleEditing}>Edit Tweet</button>
+                            </>
+                        )}
+                    </div> 
+                </>)
+            }
+        </div>
+        
+    )
+}
+
+export default Tweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,82 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { dbService } from "fbase";
+import Tweet from "components/Tweet";
 
-const Home = () => <span>Home</span>
+const Home = ({userObj}) => {
+    const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+    const [attachment, setAttachment] = useState();
+    /*const getTweets = async () => {
+        const dbTweets = await dbService.collection("tweets").get();
+        dbTweets.forEach((document) => {
+            const tweetObject = {
+                ...document.data(),
+                id:document.id
+            }
+            setTweets((prev) => [tweetObject, ...prev])
+        })
+    }*/
+    useEffect(() => {
+        dbService.collection("tweets").onSnapshot((snapshot) => {
+            const tweetArray = snapshot.docs.map((doc) => ({
+                id:doc.id,
+                ...doc.data()
+            }));
+            setTweets(tweetArray);
+        })
+    }, []);
+
+    const onChange = (event) => {
+        const {target:{value}} = event;
+        setTweet(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("tweets").add({
+            text:tweet,
+            createAt:Date.now(),
+            creatorId:userObj.uid
+        })
+        setTweet("");
+    }
+    const onFileChange = (event) => {
+        const {target:{files}} = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget:{result}} = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => setAttachment(null);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text" 
+                    value={tweet} 
+                    required 
+                    maxLength={120} 
+                    onChange={onChange} 
+                    placeholder="What's on your mind?"/>
+                <input type="file" accept="image/*" onChange={onFileChange}/>
+                <input type="submit" value="SUBMIT"/>
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px"/>
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
+            </form>
+            <div>
+                {tweets.map((tweet) => (
+                    <Tweet key={tweet.id} tweetObj={tweet} isOwner={tweet.creatorId === userObj.uid}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
 export default Home;
