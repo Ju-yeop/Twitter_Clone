1c265de43516b4a53b8471522ef5abbdc612780c
Ju-yeop <dhwnduq12@naver.com>
2022-04-11 20:42:58 +0900

    deploy

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,7 @@ import {HashRouter as Router, Route, Switch} from "react-router-dom"
 import Profile from "../routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
-import Navigation from "./components/Navigation";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ refreshUser, isLoggedIn , userObj}) => {
     return (
