4448ab0447f520044ed434e353c2de69d3565296
Ju-yeop <dhwnduq12@naver.com>
2022-04-11 20:49:25 +0900

    Updates

---
+++ precache-manifest.b2e75fd7f48ed90e38ca73fed625d744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cf451d5239b5551cb42",
+    "url": "/static/css/main.b9ec064c.chunk.css"
+  },
+  {
+    "revision": "5cf451d5239b5551cb42",
+    "url": "/static/js/main.5cf451d5.chunk.js"
+  },
+  {
+    "revision": "5a9f0cfde9dea36e3e2a",
+    "url": "/static/js/1.5a9f0cfd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5dd15c356579dd83bca1e274b56f893",
+    "url": "/index.html"
+  }
+];
